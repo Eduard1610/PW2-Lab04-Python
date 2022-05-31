b0245a8c6dfae74bab34d20b56b4f69d4959f80f
JChua21 <jchua@unsa.edu.pe>
2022-05-31 13:14:59 -0500

    Primeros avances para mostrar toda la primera linea

---
+++ Ejercicio2g.py
@@ -0,0 +1,33 @@
+from interpreter import draw 
+from chessPictures import *
+
+tableroA = [] #En este tablero se almacenaran cada objeto Picture con el color de fondo correspondiente
+casilla = square; 
+mod= 'even'; #Para pintar de color oscuro los que son modulo 0
+arreglo = [rock, knight,bishop, queen , king , bishop , knight, rock]
+#Lleno el tablero con las figuras iniciales
+i = 0;
+while i < len(arreglo):
+    if(mod == 'even'): #Para alterar el color de fondo
+        if(i % 2 == 0):
+            casilla = casilla.negative()
+        else:
+            casilla = square
+    if(mod == 'odd'):  #Para alterar el color de fondo
+        if(i % 2 == 0):
+            casilla = square
+        else:
+            casilla = casilla.negative()
+
+    pieza = casilla.__eq__(arreglo[i])
+    tableroA.append(pieza)
+    i += 1
+
+i = 1;#Lo reseteo
+#Con el tablero ya llenado realizo el dibujo uniendo cada objeto Picture
+dibujoA = tableroA[0]
+while i < len(tableroA):
+    dibujoA = dibujoA.join(tableroA[i])
+    i += 1
+
+draw(dibujoA)
