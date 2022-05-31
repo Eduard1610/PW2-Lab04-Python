8b4ecfcec86f4b2639e51a2b3568742f772262e1
JChua21 <jchua@unsa.edu.pe>
2022-05-31 13:23:14 -0500

    Se utilizo la funcionalidad previa para obtener la segunda fila

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -30,4 +30,35 @@ while i < len(tableroA):
     dibujoA = dibujoA.join(tableroA[i])
     i += 1
 
-draw(dibujoA)
+#Mismo procediiento de arriba pero esta vez se pintaran de color clar
+tableroB = []
+casilla = square;
+mod= 'odd'
+arregloB = [pawn , pawn , pawn , pawn , pawn , pawn , pawn ,pawn]
+#Lleno el tablero con las figuras iniciales
+i = 0;
+while i < len(arregloB):
+    if(mod == 'even'): #Para alterar el color de fondo
+        if(i % 2 == 0):
+            casilla = casilla.negative()
+        else:
+            casilla = square
+    if(mod == 'odd'):  #Para alterar el color de fondo
+        if(i % 2 == 0):
+            casilla = square
+        else:
+            casilla = casilla.negative()
+
+    pieza = casilla.__eq__(arregloB[i])
+    tableroB.append(pieza)
+    i += 1
+
+i = 1;#Lo reseteo
+#Con el tablero ya llenado realizo el dibujo
+dibujoB = tableroB[0]
+while i < len(tableroB):
+    dibujoB = dibujoB.join(tableroB[i])
+    i += 1
+
+
+draw(dibujoA.under(dibujoB))
